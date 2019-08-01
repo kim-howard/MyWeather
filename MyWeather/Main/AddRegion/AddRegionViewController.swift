89fe9e4ac0e7eb4679e438bd252017106af9a19b
onemoon <onemoonstudio@gmail.com>
2019-08-01 19:46:37 +0900

    FEAT : Add Region View
    FEAT
    AddRegionView base

--- MyWeather/Main/AddRegion/AddRegionViewController.swift
+++ MyWeather/Main/AddRegion/AddRegionViewController.swift
@@ -9,13 +9,35 @@
 import UIKit
 
 class AddRegionViewController: UIViewController {
-
-    @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var regionSearchBar: UISearchBar!
+    @IBOutlet weak var cancelButton: UIButton! {
+        didSet {
+            cancelButton.addTarget(self, action: #selector(didTapCancelButton(_:)), for: .touchUpInside)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setSetachBar()
+    }
+    
+    private func setSetachBar() {
+        regionSearchBar.delegate = self
+    }
+    
+    @objc func didTapCancelButton(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
     }
     
 }
+
+extension AddRegionViewController: UISearchBarDelegate {
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // 0.5 초간 반응이 없으면 검색
+        print("textDidChange")
+    }
+    
+}
+
